8d61925b9ff0521af5a7cfa84aa28fd7aca2b414
zermit45 <antoniolucchiniandrade@gmail.com>
2026-06-28 15:23:03 -0300

    Add files via upload

--- draft-patch.js
+++ draft-patch.js
@@ -67,7 +67,7 @@
     if(_promise)return _promise;
     _promise=new Promise(function(res){
       var s=document.createElement("script");
-      s.src="draft-master-players.js?v=20260628-v11-copaneutra";
+      s.src="draft-master-players.js?v=20260628-v12-copa72";
       s.onload=function(){res();};
       s.onerror=function(){_promise=null;res();}; // falhou → catálogo cai no original
       document.head.appendChild(s);
